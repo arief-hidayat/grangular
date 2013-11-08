e0e30600b3eec6e190458c4bf7220e4fc94e1a4a
arief hidayat <mr.arief.hidayat@gmail.com>
2013-11-09 07:06:46 +0800

    update angularJS to 1.2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,7 +68,7 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-        compile ":angularjs-resources:1.2.0-RC3"
+        compile ":angularjs-resources:1.2.0"
         compile ":grails-angular-template:0.1.2"
 //        test ":karma-test-runner:0.1.1"
     }
